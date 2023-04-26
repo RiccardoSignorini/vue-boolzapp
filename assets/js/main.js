f596d749e1db88f8186315c44f8b5d50d11a4179
RiccardoSignorini <riccardo.signorini1@gmail.com>
2023-04-26 12:25:53 +0200

    apertura cartella lavoro

---
+++ assets/js/main.js
@@ -0,0 +1,26 @@
+/*
+---PROBLEMA---
+Milestone 1
+Replica della grafica con la possibilità di avere messaggi scritti dall’utente (verdi) e dall’interlocutore (bianco) assegnando due classi CSS diverse
+Visualizzazione dinamica della lista contatti: tramite la direttiva v-for, visualizzare nome e immagine di ogni contatto
+*/
+
+// INTESTAZIONE VUE
+const {createApp} = Vue
+
+createApp({
+    data() {
+        return{
+            // VARIABILI
+            
+        }
+    },
+
+    created(){
+
+    },	
+
+    methods: {
+        // FUNZIONI
+    }
+}).mount('#app')
